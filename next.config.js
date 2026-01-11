0e9d4c7e12036fc9352c0a9db966847c0c4183c7
Arnav <arnav11597singh@gmail.com>
2026-01-11 19:16:22 +0530

    Downgrade to stable Next 14 + React 18 for Vercel

--- next.config.js
+++ next.config.js
@@ -3,8 +3,6 @@ const path = require('path')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone',
-  outputFileTracingRoot: path.join(__dirname, '../'),
 }
 
 module.exports = nextConfig
